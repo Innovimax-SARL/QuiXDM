fc85e759a3f295a775e4804bac7fa1a4f6109470
Innovimax <innovimax>
2015-09-29 10:39:22 +0200

    add test

---
+++ test/innovimax/quixproc/datamodel/generator/test/TestGenerator.java
@@ -0,0 +1,14 @@
+package innovimax.quixproc.datamodel.generator.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGenerator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
