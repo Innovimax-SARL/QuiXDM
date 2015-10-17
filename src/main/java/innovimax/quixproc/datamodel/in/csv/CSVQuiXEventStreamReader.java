0b3487a8279bf08178c7813476addcc608920391
innovimax <innovimax@gmail.com>
2015-10-17 21:15:18 +0200

    prepare other parser

---
+++ src/main/java/innovimax/quixproc/datamodel/in/csv/CSVQuiXEventStreamReader.java
@@ -0,0 +1,34 @@
+package innovimax.quixproc.datamodel.in.csv;
+
+import innovimax.quixproc.datamodel.QuiXException;
+import innovimax.quixproc.datamodel.event.AQuiXEvent;
+import innovimax.quixproc.datamodel.in.AQuiXEventStreamReader;
+import innovimax.quixproc.datamodel.in.AStreamSource;
+
+public class CSVQuiXEventStreamReader extends AQuiXEventStreamReader  {
+
+	@Override
+	protected AQuiXEvent load(AStreamSource current) throws QuiXException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected AQuiXEvent process(CallBack callback) throws QuiXException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reinitialize(AStreamSource current) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
