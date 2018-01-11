df02b59f937c6efe3d994f5d64ec47eb7bbd33da
innovimax <innovimax@gmail.com>
2018-01-11 11:26:24 +0100

    update source header to Apache Licence v2

--- src/main/java/innovimax/quixproc/datamodel/in/rdf/RDFQuiXEventStreamReader.java
+++ src/main/java/innovimax/quixproc/datamodel/in/rdf/RDFQuiXEventStreamReader.java
@@ -1,3 +1,10 @@
+/*
+ * QuiXProc: efficient evaluation of XProc Pipelines.
+ * Copyright (C) 2011-2018 Innovimax
+ * All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0*/
 package innovimax.quixproc.datamodel.in.rdf;
 
 import java.util.concurrent.ExecutorService;
--- src/main/java/innovimax/quixproc/datamodel/stream/IQuiXStreamReader.java
+++ src/main/java/innovimax/quixproc/datamodel/stream/IQuiXStreamReader.java
@@ -1,22 +1,10 @@
 /*
-QuiXProc: efficient evaluation of XProc Pipelines.
-Copyright (C) 2011-2018 Innovimax
-All rights reserved.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 3
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * QuiXProc: efficient evaluation of XProc Pipelines.
+ * Copyright (C) 2011-2018 Innovimax
+ * All rights reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may obtain a copy of the License at
+ *        http://www.apache.org/licenses/LICENSE-2.0*/
 package innovimax.quixproc.datamodel.stream;
 
 import java.util.NoSuchElementException;
@@ -138,7 +126,7 @@ import innovimax.quixproc.datamodel.QuiXToken;
  * </table>
  *
  * @version 0.1
- * @author Copyright (c) 2015 by Innovimax. All Rights Reserved.
+ * @author Copyright (c) 2015-2018 by Innovimax. All Rights Reserved.
  * @see QuiXToken
  * @see javax.xml.stream.XMLStreamReader
  */
